4c8a25732466e30467fd34abd1473f823a0d5235
Donal Mongey <donalmongey@gmail.com>
2018-11-14 11:37:44 +0000

    Initial commit

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from blog.views import get_unpublished_posts, write_post, read_post, edit_post, publish_post
+
+urlpatterns = [
+    path('unpublished', get_unpublished_posts, name='get_unpublished_posts'),
+    path('write/', write_post, name='write_post'),
+    path('<int:id>/', read_post, name='read_post'),
+    path('<int:id>/edit/', edit_post, name='edit_post'),
+    path('<int:id>/publish/', publish_post, name='edit_post')
+]
